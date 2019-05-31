d9819eb2c0088ca20365e1f5ab7eed4ec2b24cf0
AntonioCCruz <48328654+AntonioCCruz@users.noreply.github.com>
2019-06-01 00:31:04 +0200

    Add files via upload

---
+++ src/com/canf/www/errors/ArticleNoExistentException.java
@@ -0,0 +1,10 @@
+package com.canf.www.errors;
+
+public class ArticleNoExistentException extends ErrorException {
+	public ArticleNoExistentException(String m) {
+		super(m);
+	}
+	public ArticleNoExistentException() {
+	}
+
+}
