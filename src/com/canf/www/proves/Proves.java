b34166f3a69c089fb0d0e0726ec9968a8f4a77e0
AntonioCCruz <48328654+AntonioCCruz@users.noreply.github.com>
2019-05-31 15:41:55 +0200

    proves de magatzem

--- src/com/canf/www/proves/Proves.java
+++ src/com/canf/www/proves/Proves.java
@@ -1,10 +1,65 @@
 package com.canf.www.proves;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.canf.www.articles.Llibre;
+import com.canf.www.errors.QuantitatNoDisponibleException;
+import com.canf.www.errors.ValidacionException;
+import com.canf.www.gestio.LiniaFactura;
+import com.canf.www.gestio.Magatzem;
+import com.canf.www.gestio.Venta;
+
 public class Proves {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("0");
+		try {
+			Llibre l1 = new Llibre("1","1",1,"1","1","1",1,1);
+			Llibre l2 = new Llibre("2","2",2,"2","2","2",2,2);
+			Llibre l3 = new Llibre("3","3",3,"3","3","3",3,3);
+			Llibre l4 = new Llibre("4","4",4,"4","4","4",4,4);
+			LiniaFactura lf1 = new LiniaFactura(l1,2);
+			LiniaFactura lf2 = new LiniaFactura(l2,3);
+			LiniaFactura lf3 = new LiniaFactura(l3,1);
+			LiniaFactura lf4 = new LiniaFactura(l4,3);
+			HashMap<Integer, LiniaFactura> venta = new HashMap<Integer,LiniaFactura>();
+			venta.put(1,lf1);
+			venta.put(2,lf2);
+			
+			HashMap<Integer, LiniaFactura> venta2 = new HashMap<Integer,LiniaFactura>();
+			venta2.put(1,lf3);
+			venta2.put(2,lf4);
+		
 
+		Venta v1 = new Venta(venta, "1");
+		Venta v2 = new Venta(venta2, "2");
+	
+		
+		Magatzem m1 = new Magatzem("Espain");
+		
+		m1.afegeixVenta(v1);
+		m1.afegeixVenta(v2);
+		
+		
+		ArrayList<String> llistat = new ArrayList<String>();
+		llistat = m1.llistaVenta();
+		System.out.println("1");
+		for(String st : llistat) {
+			System.out.println("2");
+			System.out.println(st);
+		}
+
+		
+		} catch (ValidacionException | QuantitatNoDisponibleException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	
+	
+	
+	
 }
