eef129811c0658ce7d421838db13a9a5fbafa9e5
EsteveSC90 <36677541+EsteveSC90@users.noreply.github.com>
2019-06-04 19:30:59 +0200

    Add files via upload

--- src/com/canf/www/gestio/Venta.java
+++ src/com/canf/www/gestio/Venta.java
@@ -12,75 +12,81 @@ import com.canf.www.errors.DataException;
 import com.canf.www.errors.ValidacionException;
 
 public class Venta {
-	private static final AtomicInteger count = new AtomicInteger(0);//revisar
-	private HashMap<Integer, LiniaFactura> factura;
-	private String client;
-	private LocalDate data;
+    
+    private HashMap<Integer, LiniaFactura> factura;
+    private String client;
+    private LocalDate data;
 
-	public Venta(String client) {
-		super();
-		this.factura = new HashMap<Integer, LiniaFactura>();
-		this.client = client;
-		data=LocalDate.now();
-	}
+    public Venta(String client) {
+        super();
+        this.factura = new HashMap<Integer, LiniaFactura>();
+        this.client = client;
+        data = LocalDate.now();
+    }
 
-	public Venta(HashMap<Integer, LiniaFactura> factura, String client) {
-		super();
-		this.factura = factura;
-		this.client = client;
-		data=LocalDate.now();
-	}
+    public Venta(HashMap<Integer, LiniaFactura> factura, String client) {
+        super();
+        this.factura = factura;
+        this.client = client;
+        data = LocalDate.now();
+    }
 
-	public String getClient() {
-		return client;
-	}
+    public String getClient() {
+        return client;
+    }
 
-	public LocalDate getData() {
-		return data;
-	}
+    public LocalDate getData() {
+        return data;
+    }
 
-	public boolean afegeixArticle(LiniaFactura a) throws DataException {
-		
-		if(data.isEqual(LocalDate.now())) {
-				if (factura.containsValue(a)) {
-					return false;
-			} else {
-				factura.put(count.getAndIncrement(), a);
-				return true;
-			}
-		} else 
-			throw new DataException("No se pot afegir productes.");
-	}
+    public boolean afegeixArticle(LiniaFactura a) throws DataException {
 
-	public double esborraArticle(LiniaFactura a) throws ValidacionException {
-		double b = factura.get(a).getPreu() * factura.get(a).getQuantitat();
-		factura.get(a).getArticle().setStock(factura.get(a).getArticle().getStock() + factura.get(a).getQuantitat());
-		factura.remove(a);
-		return b;
-	}
+        if (data.isEqual(LocalDate.now())) {
+            if (factura.containsValue(a)) {
+                return false;
+            } else {
+                factura.put(a.getArticle().getReferencia(), a);
+                return true;
+            }
+        } else {
+            throw new DataException("No se pot afegir productes.");
+        }
+    }
 
-	public HashMap<Integer, LiniaFactura> llista() {
-		return factura;
-	}
+    public double esborraArticle(LiniaFactura a) throws ValidacionException {
+        double b = factura.get(a).getPreu() * factura.get(a).getQuantitat();
+        factura.get(a).getArticle().setStock(factura.get(a).getArticle().getStock() + factura.get(a).getQuantitat());
+        factura.remove(a);
+        return b;
+    }
 
-	public double calculaTotal() {
-		double a = 0;
-		for (int i = 0; i < factura.size(); i++) {
-			a = a + factura.get(i).getPreu() * factura.get(i).getQuantitat();
-		}
-		return a;
-	}
-	public String toXML() {
-		String txtXml = "";
-			txtXml = txtXml + "<Venta>" + "\n";
-			txtXml = txtXml + "<data>" + data + "</data>" + "\n";
-			for (int i =0; i<factura.size();i++) {
-				txtXml = txtXml + factura.get(i).toXML()+ "\n";
-			}
-			txtXml = txtXml + "<client>" + client + "</client> " + "\n";
-			txtXml = txtXml + "<total>" + calculaTotal() + "</total>"+"\n";
-			txtXml = txtXml + "</Venta>";
-		return txtXml;
-	}
+    public HashMap<Integer, LiniaFactura> llista() {
+        return factura;
+    }
+
+    public double calculaTotal() {
+        double a = 0;
+        for (int i = 0; i < factura.size(); i++) {
+            a = a + factura.get(i).getPreu() * factura.get(i).getQuantitat();
+        }
+        return a;
+    }
+
+    public String toXML() {
+        String txtXml = "";
+        int cont = 0;
+        txtXml = txtXml + "<Venta>" + "\n";
+        txtXml = txtXml + "<data>" + data + "</data>" + "\n";
+        for (int i = 0; i < factura.size(); i++) {
+            cont = i++;
+            txtXml = txtXml + "<linia>" + cont + "</linia>" + "\n";
+            txtXml = txtXml + factura.get(i).toXML() + "\n";
+
+        }
+        txtXml = txtXml + "<client>" + client + "</client> " + "\n";
+        txtXml = txtXml + "<total>" + calculaTotal() + "</total>" + "\n";
+        txtXml = txtXml + "</Venta>";
+        return txtXml;
+    }
 
 }
